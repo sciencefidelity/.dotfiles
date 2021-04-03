e5eda945a0ac580892c76864410e00e2d3a2b7d5
Matt Cook <32623301+sciencefidelity@users.noreply.github.com>
2021-04-03 02:57:35 +0100

    Add files via upload

---
+++ mattbox.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#F1EDDA", "#CA2726", "#989629", "#D69831", "#488588", "#B06386", "#6A9C6C", "#A89985", "#928375", "#F94B3D", "#B8BA37", "#F9BC41", "#82A296", "#D2879B", "#90BF7F", "#EBDBB4"]);
+t.prefs_.set('foreground-color', "#ebdbb2");
+t.prefs_.set('background-color', "#F1EDDA");
+t.prefs_.set('cursor-color', 'rgba(252,252,252,0.3)');
