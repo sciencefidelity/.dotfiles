2728d3b3fbcf7cfdac9f99af882a7b5fba2f4c96
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-04 22:00:41 +0000

    Change file structure

--- mattbox.js
+++
@@ -1,4 +0,0 @@
-t.prefs_.set('color-palette-overrides',["#282828", "#CA2726", "#989629", "#D69831", "#488588", "#B06386", "#6A9C6C", "#A89985", "#928375", "#F94B3D", "#B8BA37", "#F9BC41", "#82A296", "#D2879B", "#90BF7F", "#EBDBB4"]);
-t.prefs_.set('foreground-color', "#F1EDDA");
-t.prefs_.set('background-color', "#1B1C1D");
-t.prefs_.set('cursor-color', 'rgba(252,252,252,0.3)');
