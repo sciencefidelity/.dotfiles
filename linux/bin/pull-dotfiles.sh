551fd5246806e892493e2f39fc3c3ce5eab25188
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-09 13:27:04 +0100

    Add scripts for keep in dotfiles in sync

---
+++ linux/bin/pull-dotfiles.sh
@@ -0,0 +1,12 @@
+#! bin/bash
+
+cp /home/matt/dotfiles/linux/.bash_profile /home/matt/
+cp /home/matt/dotfiles/linux/.dir_colors /home/matt/
+cp /home/matt/dotfiles/linux/.vimrc /home/matt/
+cp /home/matt/dotfiles/linux/.zshrc /home/matt/
+
+sudo cp /home/matt/dotfiles/linux/.bash_profile /root/
+sudo cp /home/matt/dotfiles/linux/.dir_colors /root/
+sudo cp /home/matt/dotfiles/linux/.vimrc /root/
+sudo cp /home/matt/dotfiles/linux/.zshrc /root/
+
---
+++ linux/bin/push-dotfiles.sh
@@ -0,0 +1,12 @@
+#! bin/bash
+
+cp /home/matt/.bash_profile /home/matt/dotfiles/linux/
+cp /home/matt/.dir_colors /home/matt/dotfiles/linux/
+cp /home/matt/.vimrc /home/matt/dotfiles/linux/
+cp /home/matt/.zshrc /home/matt/dotfiles/linux/
+
+sudo cp /home/matt/.bash_profile /root/
+sudo cp /home/matt/.dir_colors /root/
+sudo cp /home/matt/.vimrc /root/
+sudo cp /home/matt/.zshrc /root/
+
