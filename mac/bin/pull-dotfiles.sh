e41c87da7f2d6e1f2f78464f061711e5d915ba67
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-04-10 20:55:02 +0000

    Scripts added for synchronising dotfiles

---
+++ mac/bin/pull-dotfiles.sh
@@ -0,0 +1,6 @@
+#! bin/bash
+
+cp /Users/matt/dotfiles/mac/.bash_profile /Users/matt/
+cp /Users/matt/dotfiles/mac/.dir_colors /Users/matt/
+cp /Users/matt/dotfiles/mac/.vimrc /Users/matt/
+cp /Users/matt/dotfiles/mac/.zshrc /Users/matt/
