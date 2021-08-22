77a81d175bc1031a9740e47780aef95ced0282bb
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-22 17:24:37 +0000

    Change vim to nvim

--- linux/bin/pull-dotfiles.sh
+++ linux/bin/pull-dotfiles.sh
@@ -1,13 +1,6 @@
 #! bin/bash
 
+cp /home/matt/dotfiles/linux/.zshrc /home/matt/
 cp /home/matt/dotfiles/linux/.bash_profile /home/matt/
 cp /home/matt/dotfiles/linux/.dir_colors /home/matt/
-cp /home/matt/dotfiles/linux/.vimrc /home/matt/
-cp /home/matt/dotfiles/linux/.zshrc /home/matt/
 cp -R /home/matt/dotfiles/linux/nvim /home/matt/.config/
-
-sudo cp /home/matt/dotfiles/linux/.bash_profile /root/
-sudo cp /home/matt/dotfiles/linux/.dir_colors /root/
-sudo cp /home/matt/dotfiles/linux/.vimrc /root/
-sudo cp /home/matt/dotfiles/linux/.zshrc /root/
-
--- linux/bin/push-dotfiles.sh
+++ linux/bin/push-dotfiles.sh
@@ -1,13 +1,6 @@
 #! bin/bash
 
+cp /home/matt/.zshrc /home/matt/dotfiles/linux/
 cp /home/matt/.bash_profile /home/matt/dotfiles/linux/
 cp /home/matt/.dir_colors /home/matt/dotfiles/linux/
-cp /home/matt/.vimrc /home/matt/dotfiles/linux/
-cp /home/matt/.zshrc /home/matt/dotfiles/linux/
 cp -R /home/matt/.config/nvim /home/matt/dotfiles/linux/
-
-sudo cp /home/matt/.bash_profile /root/
-sudo cp /home/matt/.dir_colors /root/
-sudo cp /home/matt/.vimrc /root/
-sudo cp /home/matt/.zshrc /root/
-
