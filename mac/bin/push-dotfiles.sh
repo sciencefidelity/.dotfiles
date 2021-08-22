7fee4bba936125f3dc4f6db1a3403cc0fdc955c0
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-22 17:25:23 +0000

    Reorder

--- mac/bin/push-dotfiles.sh
+++ mac/bin/push-dotfiles.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 
+cp /Users/matt/.zshrc /Users/matt/dotfiles/mac/
 cp /Users/matt/.bash_profile /Users/matt/dotfiles/mac/
 cp /Users/matt/.dir_colors /Users/matt/dotfiles/mac/
 cp /Users/matt/.vimrc /Users/matt/dotfiles/mac/
-cp /Users/matt/.zshrc /Users/matt/dotfiles/mac/
