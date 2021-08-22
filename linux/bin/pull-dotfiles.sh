59465b8e115e731ed5ed414535ce6365c806d565
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-22 14:04:14 +0100

    Add neovim config to scripts

--- linux/bin/pull-dotfiles.sh
+++ linux/bin/pull-dotfiles.sh
@@ -4,6 +4,7 @@ cp /home/matt/dotfiles/linux/.bash_profile /home/matt/
 cp /home/matt/dotfiles/linux/.dir_colors /home/matt/
 cp /home/matt/dotfiles/linux/.vimrc /home/matt/
 cp /home/matt/dotfiles/linux/.zshrc /home/matt/
+cp -R /home/matt/dotfiles/linux/nvim /home/matt/.config/
 
 sudo cp /home/matt/dotfiles/linux/.bash_profile /root/
 sudo cp /home/matt/dotfiles/linux/.dir_colors /root/
--- linux/bin/push-dotfiles.sh
+++ linux/bin/push-dotfiles.sh
@@ -4,6 +4,7 @@ cp /home/matt/.bash_profile /home/matt/dotfiles/linux/
 cp /home/matt/.dir_colors /home/matt/dotfiles/linux/
 cp /home/matt/.vimrc /home/matt/dotfiles/linux/
 cp /home/matt/.zshrc /home/matt/dotfiles/linux/
+cp -R /home/matt/.config/nvim /home/matt/dotfiles/linux/
 
 sudo cp /home/matt/.bash_profile /root/
 sudo cp /home/matt/.dir_colors /root/
