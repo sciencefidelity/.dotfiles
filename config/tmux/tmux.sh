1069f7c88688e87cbb122fc945c0956dc7ac9bf7
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-26 22:29:08 +0000

    Fix typo

--- config/tmux/tmux.sh
+++ config/tmux/tmux.sh
@@ -1,6 +1,6 @@
 tbtm () {
   if ! tmux has-session -t $1; then
-  tumx new -s $1 -d
+  tmux new -s $1 -d
     tmux split-window -t $1:1 -v -p 15
     tmux send-keys -t $1:1.0 vim Enter
     tmux send-keys -t $1:1.1 \
