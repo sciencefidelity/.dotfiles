0aa71e6315aaa48a94cb7effdacc56f93d3ac2a6
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-26 22:31:23 +0000

    Add space in command

--- config/tmux/tmux.sh
+++ config/tmux/tmux.sh
@@ -1,10 +1,10 @@
 tbtm () {
   if ! tmux has-session -t $1; then
-  tmux new -s $1 -d
+    tmux new -s $1 -d
     tmux split-window -t $1:1 -v -p 15
     tmux send-keys -t $1:1.0 vim Enter
     tmux send-keys -t $1:1.1 \
-      npm run dev Enter
+      "npm run dev" Enter
   fi
   tmux attach -t $1
 }
