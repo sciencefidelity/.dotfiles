9c702234c282e538807054342fc7e6ecf3e977e4
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-27 10:39:40 +0000

    Set up temperature in status bar

--- config/utils/temp.c
+++ config/utils/temp.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
   FILE *fp;
 
   int temp = 0;
   fp = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
   fscanf(fp, "%d", &temp);
-  printf(">> CPU Temp: %.2f°C\n", temp / 1000.0);
+  printf("%.2f°C\n", temp / 1000.0);
   fclose(fp);
 
   return 0;
 }
-
