a9877513cc8e59a6adfe5f432832e2d29c86e6b1
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-27 20:54:55 +0100

    Add basic settings for emacs

--- config/emacs.d/init.el
+++ config/emacs.d/init.el
@@ -1,4 +1,8 @@
-(menu-bar-mode -1)     ; Disable to menu bar
+(menu-bar-mode -1)             ; disable to menu bar
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'dracula t)
+(setq dracula-alternate-mode-line-and-minibuffer t)
+(load-theme 'dracula t)        ; load theme
+
+(setq backup-inhibited t)      ; disable backups
+(setq auto-save-default nil)   ; disable autosave
