5016f4f46ac91d19d76a37680740be5086e9c571
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-28 03:36:23 +0100

    Set up evil mode

--- config/emacs.d/init.el
+++ config/emacs.d/init.el
@@ -1,13 +1,13 @@
-(menu-bar-mode -1)             ; disable to menu bar
+(menu-bar-mode -1)            ; disable to menu bar
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (setq dracula-alternate-mode-line-and-minibuffer t)
-(load-theme 'dracula t)        ; load theme
+(load-theme 'dracula t)       ; load theme
 
-(setq backup-inhibited t)      ; disable backups
-(setq auto-save-default nil)   ; disable autosave
+(setq backup-inhibited t)     ; disable backups
+(setq auto-save-default nil)  ; disable autosave
 
-(column-number-mode)           ; line and column numbers
+(column-number-mode)          ; line and column numbers
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
 ; show invisible characters
@@ -20,3 +20,16 @@
           (tab-mark 9 [8594 9] [92 9])
           )))
 (add-hook 'prog-mode-hook 'whitespace-mode)
+
+(require 'package)            ; initialte package manager
+  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)               ; set up evil mode
+(evil-mode 1)
+
+(add-to-list·'load-path·"~/.emacs.d/undo-tree")
+(global-undo-tree-mode)       ; set up undo-tree
+
+
