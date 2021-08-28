011a3a6851d48515f8f1b1bc8f1fa9c61c2f6eea
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-28 11:18:39 +0100

    Add basic emacs functionality

--- config/emacs.d/init.el
+++ config/emacs.d/init.el
@@ -1,7 +1,7 @@
 (menu-bar-mode -1)            ; disable to menu bar
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(setq dracula-alternate-mode-line-and-minibuffer t)
+;(setq dracula-alternate-mode-line-and-minibuffer t)
 (load-theme 'dracula t)       ; load theme
 
 (setq backup-inhibited t)     ; disable backups
@@ -9,6 +9,9 @@
 
 (column-number-mode)          ; line and column numbers
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+(setq-default indent-tabs-mode nil) ; disable tabs
+(setq tab-width 2)            ; set default tab width
 
 ; show invisible characters
 (progn
@@ -21,15 +24,22 @@
           )))
 (add-hook 'prog-mode-hook 'whitespace-mode)
 
-(require 'package)            ; initialte package manager
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
+(add-to-list 'load-path "~/.emacs.d/rainbow-delimiters")
+(require 'rainbow-delimiters) ; load rainbow delimiters
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+(add-to-list 'load-path "~/.emacs.d/highlight-indent-guides")
+;(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+
+;(require 'package)            ; initialte package manager
+;  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;(package-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/evil")
 (require 'evil)               ; set up evil mode
 (evil-mode 1)
 
-(add-to-list·'load-path·"~/.emacs.d/undo-tree")
-(global-undo-tree-mode)       ; set up undo-tree
-
+;(add-to-list 'load-path "~/.emacs.d/undo-tree")
+;(require 'undo-tree)
+;(global-undo-tree-mode)       ; set up undo-tree
 
