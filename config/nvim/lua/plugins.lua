65a5990e91b2900dc009fdc31d4675580b5591a9
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-08-30 20:04:27 +0100

    Clean up imports

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -2,36 +2,46 @@ local execute = vim.api.nvim_command
 local fn = vim.fn
 
 return require('packer').startup(function()
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim' -- Package manager
-  use 'tpope/vim-fugitive' -- Git commands in nvim
-  use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
-  use 'tpope/vim-commentary' -- "gc" to comment visual regions/lines
-  -- use 'ludovicchabant/vim-gutentags' -- Automatic tags management
-  -- UI to select things (files, grep results, open buffers...)
+  --·https://github.com/wbthomason/packer.nvim
+  use 'wbthomason/packer.nvim'
+  -- https://github.com/tpope/vim-fugitive
+  use 'tpope/vim-fugitive'
+  -- https://github.com/tpope/vim-rhubarb
+  use 'tpope/vim-rhubarb'
+  -- https://github.com/tpope/vim-commentary
+  use 'tpope/vim-commentary'
+  -- https://github.com/ludovicchabant/vim-gutentags
+  -- use 'ludovicchabant/vim-gutentags'
+  -- https://github.com/nvim-telescope/telescope.nvim
   use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } } }
-  -- use 'Mofiqul/dracula.nvim' -- Theme
-  use 'dracula/vim' -- Theme
-  -- use 'joshdick/onedark.vim' -- Theme inspired by Atom
-  use 'itchyny/lightline.vim' -- Fancier statusline
-  -- Add indentation guides even on blank lines
+  -- https://github.com/dracula/vim
+  use 'dracula/vim'
+  -- https://github.com/itchyny/lightline.vim
+  use 'itchyny/lightline.vim'
+  -- https://github.com/lukas-reineke/indent-blankline.nvim
   use 'lukas-reineke/indent-blankline.nvim'
-  -- Add git related info in the signs columns and popups
+  -- https://github.com/lewis6991/gitsigns.nvim
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  -- https://github.com/nvim-treesitter/nvim-treesitter
   use 'nvim-treesitter/nvim-treesitter'
-  use 'nvim-treesitter/nvim-treesitter-textobjects' -- Additional textobjects for treesitter
-  use { 'kyazdani42/nvim-tree.lua', 'kyazdani42/nvim-web-devicons' }
-  use 'neovim/nvim-lspconfig' -- LSP config
+  -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  use 'nvim-treesitter/nvim-treesitter-textobjects'
+  -- https://github.com/kyazdani42/nvim-tree.lua
+  use 'kyazdani42/nvim-tree.lua'
+  -- https://github.com/neovim/nvim-lspconfig
+  use 'neovim/nvim-lspconfig'
+  -- https://github.com/glepnir/lspsaga.nvim
   use 'glepnir/lspsaga.nvim'
+  -- https://github.com/hrsh7th/nvim-compe
   use 'hrsh7th/nvim-compe'
+  -- https://github.com/hrsh7th/vim-vsnip
   use 'hrsh7th/vim-vsnip'
-  use 'pangloss/vim-javascript' -- JavaScript syntax highlighting and improved indentation
+  -- use 'pangloss/vim-javascript' -- JavaScript syntax highlighting and improved indentation
   -- Flutter tools
-  use 'dart-lang/dart-vim-plugin'
-  use { 'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim' }
+  -- use 'dart-lang/dart-vim-plugin'
+  -- use { 'akinsho/flutter-tools.nvim', requires = 'nvim-lua/plenary.nvim' }
   -- Go tools
-  use 'fatih/vim-go'
+  -- use 'fatih/vim-go'
   -- use 'ray-x/go.nvim'
-  -- Highlight kitty.conf
-  use 'fladson/vim-kitty'
+  use 'fladson/vim-kitty' -- Highlighting for Kitty.conf
 end)
