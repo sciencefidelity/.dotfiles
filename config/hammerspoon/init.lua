6ecf247d03a4f20d6c4d8c583c53b8ff06f59021
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-09-02 22:43:48 +0000

    Prettify

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -24,49 +24,58 @@ end)
 hs.hotkey.bind({"cmd", "ctrl"}, "b", function()
   hs.application.open('Brave Browser.app')
 end)
+
 -- open Dash with "cmd+ctrl+d"
 hs.hotkey.bind({"cmd", "ctrl"}, "d", function()
   hs.application.open('Dash.app')
 end)
+
 -- open Firefox with "cmd+ctrl+f"
 hs.hotkey.bind({"cmd", "ctrl"}, "f", function()
   hs.application.open('Firefox.app')
 end)
+
 -- open Figma with "cmd+ctrl+g"
 hs.hotkey.bind({"cmd", "ctrl"}, "f", function()
   hs.application.open('Figma.app')
 end)
+
 -- open Insomnia with "cmd+ctrl+i"
 hs.hotkey.bind({"cmd", "ctrl"}, "i", function()
   hs.application.open('Insomnia.app')
 end)
+
 -- open Kitty with "cmd+ctrl+k"
 hs.hotkey.bind({"cmd", "ctrl"}, "k", function()
   hs.application.open('Kitty.app')
 end)
+
 -- open Mail with "cmd+ctrl+m"
 hs.hotkey.bind({"cmd", "ctrl"}, "m", function()
   hs.application.open('Mail.app')
 end)
+
 -- open Nova with "cmd+ctrl+n"
 hs.hotkey.bind({"cmd", "ctrl"}, "n", function()
   hs.application.open('Nova.app')
 end)
+
 -- open Notion with "cmd+ctrl+o"
 hs.hotkey.bind({"cmd", "ctrl"}, "o", function()
   hs.application.open('Notion.app')
 end)
+
 -- open Spotify with "cmd+ctrl+p"
 hs.hotkey.bind({"cmd", "ctrl"}, "p", function()
   hs.application.open('Spotify.app')
 end)
+
 -- open Safari with "cmd+ctrl+s"
 hs.hotkey.bind({"cmd", "ctrl"}, "s", function()
   hs.application.open('Safari.app')
 end)
+
 -- open TextEdit with "cmd+ctrl+t"
 hs.hotkey.bind({"cmd", "ctrl"}, "t", function()
   hs.application.open('TextEdit.app')
 end)
-
-
