207055bb47c71183e810cf568dcd71cdc0ebe086
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-09-02 22:04:38 +0000

    Add reload script

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,3 +1,17 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.alert.show("Hello World!")
-end)
+-- http://www.hammerspoon.org/
+
+-- reload config each time this file changes
+function reloadConfig(files)
+  doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
+    end
+  end
+  if doReload then
+    hs.reload()
+  end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
+
