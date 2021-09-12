74430c9d30622bc2bc5379ea8f3cd49c257149e5
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-09-12 18:30:59 +0000

    Change packer install path

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,6 +1,5 @@
 -- Install packer
--- local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-local install_path = vim.fn.stdpath 'data' .. '/pack/start/packer.nvim'
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
