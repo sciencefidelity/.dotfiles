fc4d510afea72b664edc1718c3ccced0e00eb28f
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-10-15 19:38:38 +0100

    feat: add configs for Blink Shell

---
+++ config/blink/dracula.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#21222C", "#ff5555", "#50fa7b", "#f1fa8c", "#bd93f9", "#ff79c6", "#8be9fd", "#F8F8F2", "#6272A4", "#FF6E6E", "#69FF94", "#FFFFA5", "#D6ACFF", "#FF92DF", "#A4FFFF", "#ffffff"]);
+t.prefs_.set('foreground-color', "#f8f8f2");
+t.prefs_.set('background-color', "#282a36");
+t.prefs_.set('cursor-color', 'rgba(187,187,187,0.5)');
