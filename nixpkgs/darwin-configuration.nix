7dd5b4f212407ce43c2ccdaa8e9c9f3e287142e5
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-08 23:19:38 +0000

    build: add basic nix-darwin configuration

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -1,15 +1,32 @@
 { config, pkgs, ... }:
 
 {
+  users.users.matt = {
+    home = "Users/matt";
+    description = "Matt Cook";
+    shell. pkgs.zsh;
+  };
+
   environment.systemPackages = with pkgs; [
     git
     vim
     wget
     zsh
   ];
+  environment.shells = [ pkgs.zsh ];
 
-  services.nix-daemon.enable = true;
+  programs.nix-index.enable = true;
 
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.launchanim = false;
+  system.defaults.dock.mineffect = "scale";
+  system.defaults.dock.orientation = "bottom";
+  system.defaults.dock.show-process-indicators = true;
+  system.defaults.dock.show-recents = false;
+  system.defaults.dock.tilesize = 64;
+
+  services.nix-daemon.enable = false;
+  nix.useDaemon = false;
   programs.zsh.enable = true;
   system.stateVersion = 4;
 }
