76f49342dfdc495b2f04fdbe2a853efc759597da
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-09 20:10:27 +0000

    feat: copy home manager config into nix-darwin

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  imports = [ <home-manager/nix-darwin> ];
+
   users.users.matt = {
     home = "Users/matt";
     description = "Matt Cook";
@@ -9,17 +11,27 @@
 
   environment.systemPackages = with pkgs; [
     bat
+    bc
+    coreutils
+    curl
     delta
     emacs
+    exa
+    fd
     fzf
     gh
     git
-    home-manager
+    gnupg
     htop
+    jq
+    lazygit
     lf
     neovim
+    pinentry-mac
     ripgrep
     starship
+    tmux
+    tree
     wget
     zsh
     zsh-autosuggestions
@@ -41,51 +53,437 @@
       "homebrew/cask-versions"
       "homebrew/core"
       "homebrew/services"
-      "buo/cask-upgrade"
-      "dart-lang/dart"
-      "sass/sass"
+      # "buo/cask-upgrade"
+      # "dart-lang/dart"
+      # "sass/sass"
     ];
 
     casks = [
-      "android-studio"
-      "bartender"
-      "brave-browser"
-      "dash"
-      "figma"
-      "firefox"
-      "hammerspoon"
-      "insomnia"
+      # "android-studio"
+      # "bartender"
+      # "brave-browser"
+      # "dash"
+      # "figma"
+      # "firefox"
+      # "hammerspoon"
+      # "insomnia"
       "karabiner-elements"
       "kitty"
-      "miro"
+      # "miro"
       "notion"
       "nova"
-      "rocket"
-      "slack"
-      "spotify"
+      # "rocket"
+      # "slack"
+      # "spotify"
     ];
 
     masApps = {
-      "Affinity Designer" = 824171161;
-      "Affinity Photo" = 824183456;
-      "Affinity Publisher" = 881418622;
-      "Compressor" = 424390742;
-      "Final Cut Pro" = 424389933;
-      "iA Writer" = 775737590;
-      "Logic Pro" = 634148309;
-      "Microsoft Excel" = 462058435;
-      "Microsoft PowerPoint" = 462062816;
-      "Microsoft Word" = 462054704;
-      "Motion" = 434290957;
-      "OneDrive" = 823766827;
-      "Pocket" = 568494494;
+      # "Affinity Designer" = 824171161;
+      # "Affinity Photo" = 824183456;
+      # "Affinity Publisher" = 881418622;
+      # "Compressor" = 424390742;
+      # "Craft - Docs and Notes Editor" = 1487937127;
+      # "Final Cut Pro" = 424389933;
+      # "iA Writer" = 775737590;
+      # "Logic Pro" = 634148309;
+      # "Microsoft Excel" = 462058435;
+      # "Microsoft PowerPoint" = 462062816;
+      # "Microsoft Word" = 462054704;
+      # "Motion" = 434290957;
+      # "OneDrive" = 823766827;
+      # "Pocket" = 568494494;
       "Refined GitHub" = 1519867270;
       "Save to Raindrop.io" = 1549370672;
-      "Slack for Desktop" = 803453959;
+      # "Slack for Desktop" = 803453959;
       "Vimari" = 1480933944;
-      "Xcode" = 497799835;
+      # "Xcode" = 497799835;
     };
   };
+
+  home-manager.useGlobalPkgs = true;
+
+  home-manager.users.matt = { pkgs, ... }: {
+    home.file.".emacs.d/init.el" = {
+      source = /home/matt/dotfiles/config/emacs.d/init.el;
+    };
+
+    home.file.".npmrc" = {
+      source = /home/matt/dotfiles/config/npm/.npmrc;
+    };
+
+    environment = {
+      shells = [ pkgs.zsh ];
+      shellAliases = {
+        sudo = "sudo -i";
+        poweroff = "sudo poweroff";
+        reboot = "sudo reboot";
+        sysrs = "sudo nixos-rebuild switch";
+        sysup = "sudo nixos-rebuild switch --upgrade";
+        sysclean = "sudo nix-collect-garbage -d; and sudo nix-store --optimise";
+        ls = "exa -F --group-directories-first";
+        l = "exa -aF --group-directories-first";
+        la = "exa -laF --group-directories-first --git";
+        ll = "exa -lF --group-directories-first --git";
+        lt = "exa -T --git-ignore";
+        lr = "exa -R --git-ignore";
+        mkdir = "mkdir -p";
+        df = "df -kTh";
+        free = "free -h";
+        du = "du -h -c";
+        cat = "bat";
+        grep = "rg";
+        fd = "fdfind";
+        emacs = "TERM=xterm-24bits emacs -nw";
+        push = "git push";
+        pull="git fetch origin; git merge origin/main";
+        gst = "git status";
+        cleanup = "find . -name '*.DS_Store' -type f -ls -delete";
+      };
+    };
+
+    programs.bat = {
+      enable = true;
+      config = {
+        theme = "Dracula";
+        italic-text = "always";
+        style = "full";
+        # map-syntax = [ ".eslintignore:Git Ignore" ".prettierignore:Git Ignore" ".prettierrc:JSON" ];
+      };
+      themes = {
+        dracula = builtins.readFile (pkgs.fetchFromGitHub {
+          owner = "dracula";
+          repo = "sublime"; # Bat uses sublime syntax for its themes
+          rev = "26c57ec282abcaa76e57e055f38432bd827ac34e";
+          sha256 = "019hfl4zbn4vm4154hh3bwk6hm7bdxbr1hdww83nabxwjn99ndhv";
+        } + "/Dracula.tmTheme");
+      };
+    };
+
+    programs.emacs = {
+      enable = true;
+    };
+
+    programs.fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    programs.htop = {
+      enable = true;
+    };
+
+    programs.kitty = {
+      enable = false;
+      extraConfig = ''
+        ${builtins.readFile /Users/matt/dotfiles/config/kitty/kitty.conf}
+      '';
+    };
+
+    programs.neovim = {
+      enable = true;
+      extraConfig = ''
+        lua << EOF
+        ${builtins.readFile /Users/matt/dotfiles/config/nvim/init.lua}
+        EOF
+      '';
+      withNodeJs = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+
+    programs.starship = {
+      enable = true;
+      enableZshIntegration = true;
+      settings = {
+        add_newline = false;
+
+        format = lib.concatStrings [
+          "$username"
+          "$shlvl"
+          "$singularity"
+          "$kubernetes"
+          "$directory"
+          "$hostname"
+          "$vcsh"
+          "$git_branch"
+          "$git_commit"
+          "$git_state"
+          "$git_metrics"
+          "$git_status"
+          "$hg_branch"
+          "$docker_context"
+          "$package"
+          "$cmake"
+          "$cobol"
+          "$dart"
+          "$deno"
+          "$dotnet"
+          "$elixir"
+          "$elm"
+          "$erlang"
+          "$golang"
+          "$helm"
+          "$java"
+          "$julia"
+          "$kotlin"
+          "$lua"
+          "$nim"
+          "$nodejs"
+          "$ocaml"
+          "$perl"
+          "$php"
+          "$pulumi"
+          "$purescript"
+          "$python"
+          "$rlang"
+          "$red"
+          "$ruby"
+          "$rust"
+          "$scala"
+          "$swift"
+          "$terraform"
+          "$vlang"
+          "$vagrant"
+          "$zig"
+          "$nix_shell"
+          "$conda"
+          "$memory_usage"
+          "$aws"
+          "$gcloud"
+          "$openstack"
+          "$env_var"
+          "$crystal"
+          "$custom"
+          "$cmd_duration"
+          "$line_break"
+          "$jobs"
+          "$battery"
+          "$time"
+          "$status"
+          "$shell"
+          "$character"
+        ];
+
+        aws.disabled = true;
+        battery.disabled = true;
+
+        character = {
+          success_symbol = "[➜](bold green)";
+          error_symbol = "[➜](bold red)";
+          vicmd_symbol = "[➜](bold purple)";
+        };
+
+        cmake.disabled = true;
+        conda.disabled = true;
+        crystal.disabled = true;
+
+        directory = {
+          format = "in [$path](bold cyan) ";
+        };
+
+        docker_context.disabled = true;
+        dotnet.disabled = true;
+        gcloud.disabled = true;
+        helm.disabled = true;
+
+        hostname = {
+          ssh_only = false;
+          format = "on [$hostname](bold blue) ";
+          trim_at = ".";
+          disabled = false;
+        };
+
+        java.disabled = true;
+        julia.disabled = true;
+        hg_branch.disabled = true;
+
+        nodejs = {
+          symbol = "⬢ ";
+        };
+
+        openstack.disabled = true;
+
+        package = {
+          format = "is [$version](bold red) ";
+        };
+
+        perl.disabled = true;
+        php.disabled = true;
+        rlang.disabled = true;
+        red.disabled = true;
+        ruby.disabled = true;
+        scala.disabled = true;
+        singularity.disabled = true;
+        terraform.disabled = true;
+
+        username = {
+          style_user = "yellow bold";
+          style_root = "red bold";
+          format = "[$user]($style) ";
+          disabled = false;
+          show_always = true;
+        };
+
+        vagrant.disabled = true;
+        vlang.disabled = true;
+        vcsh.disabled = true;
+        zig.disabled = true;
+      };
+    };
+
+    programs.tmux = {
+      enable = true;
+      clock24 = true;
+      escapeTime = 20;
+      extraConfig = ''
+        # fix colors on truecolor terminals
+        if-shell "uname | grep -q Darwin && echo $TERM | grep -q xterm-256color" {
+          # set -g default-terminal screen-256color
+          # set -ga terminal-overrides ",xterm-256color*:Tc"
+          set-option default-terminal "tmux-256color"
+          set-option -a terminal-overrides ",xterm-256color:RGB"
+        }
+
+        if-shell "uname | grep -q Darwin && echo $TERM | grep -q xterm-kitty" {
+          set-option default-terminal "tmux-256color"
+          set-option -a terminal-overrides ",xterm-256color:RGB"
+        }
+
+        # unbind default prefix and set it to Ctrl+s
+        set -g prefix C-s
+        unbind C-b
+        bind C-s send-prefix
+
+        # speed up escape in nvim
+        set -g base-index 1
+        set -g escape-time 20
+
+        # custom key bindings
+        bind -n M-h select-pane -L
+        bind -n M-j select-pane -D
+        bind -n M-k select-pane -U
+        bind -n M-l select-pane -R
+
+        bind-key t set-option status
+
+        # status bar options
+        set -g status-bg "#282a36"
+        set -g status-fg "#ff79c6"
+
+        set-option -g status-right ""
+        set-option -ag status-right " #[fg="#f1fa8c",bg=default]#(zsh ~/.config/zsh/cpu.sh) "
+        set-option -ag status-right " #[fg="#8be9fd",bg=default]#(free -h | awk '/^Mem/ {print $3}')/#(free -h | awk '/^Mem/ {print $2}') "
+        set-option -ag status-right " #[fg="#50fa7b",bg=default]#(zsh ~/.config/zsh/temp.sh) "
+        set-option -ag status-right " #[fg="#ff79c6",bg=default]#(date +"%R") "
+      '';
+      keyMode = "vi";
+      newSession = true;
+      terminal = "xterm-24bits";
+    };
+
+    programs.zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      autocd = true;
+      # defaultKeymap = "vicmd"; # prints a list of keymappings when starting shell
+      # dotDir = ".config/zsh";
+      history = {
+        save = 1000;
+        size = 1000;
+      };
+
+      initExtra = ''
+        # Basic auto/tab complete
+        _comp_options+=(globdots)
+
+        # vi mode
+        bindkey -v
+        export KEYTIMEOUT=1
+
+        # Use vim keys in tab complete menu
+        bindkey -M menuselect "h" vi-backward-char
+        bindkey -M menuselect "k" vi-up-line-or-history
+        bindkey -M menuselect "l" vi-forward-char
+        bindkey -M menuselect "j" vi-down-line-or-history
+        bindkey -v "^?" backward-delete-char
+
+        # Use lf to switch directories and bind it to ctrl-o
+        lfcd () {
+            tmp="$(mktemp)"
+            lf -last-dir-path="$tmp" "$@"
+            if [ -f "$tmp" ]; then
+                dir="$(cat "$tmp")"
+                rm -f "$tmp"
+                [ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
+            fi
+        }
+        bindkey -s "^o" "lfcd\n"
+
+        # Edit line in vim with ctrl-e
+        autoload edit-command-line; zle -N edit-command-line
+        bindkey "^e" edit-command-line
+
+        bindkey "^ " autosuggest-accept
+
+        export BAT_THEME="Dracula"
+
+        alias ..="cd .."
+        alias ...="cd ../.."
+        alias ....="cd ../../.."
+        alias .....="cd ../../../.."
+
+        eval "$(ssh-agent -s)" > /dev/null
+        ssh-add ~/.ssh/github 2> /dev/null
+        export GPG_TTY=$(tty)
+
+        # Git aliases
+        gitpush() {
+          git pull
+          git add .
+          git commit -m "$*"
+          git push
+        }
+        alias gp=gitpush
+
+        # archive extractor - usage: ext <file>
+        ext ()
+        {
+          if [ -f $1 ] ; then
+            case $1 in
+              *.tar.bz2) tar xjf $1;;
+              *.tar.gz) tar xzf $1;;
+              *.tar.xz) tar xJf $1;;
+              *.bz2) bunzip2 $1;;
+              *.rar) unrar x $1;;
+              *.gz) gunzip $1 ;;
+              *.tar) tar xf $1 ;;
+              *.tbz2) tar xjf $1;;
+              *.tgz) tar xzf $1;;
+              *.zip) unzip $1;;
+              *.Z) uncompress $1;;
+              *.7z) 7z x $1;;
+              *) echo "'$1' cannot be extracted via ex()";;
+            esac
+          else
+            echo "'$1' is not a valid file"
+          fi
+        }
+        export PATH="$HOME/.npm-globals/bin:$PATH"
+      '';
+
+      initExtraBeforeCompInit = ''
+        zstyle ':completion:*' menu select
+        zmodload zsh/complist
+      '';
+
+      initExtraFirst = ''
+        if [[ "$TERM" == "xterm-256color" ]]; then
+            export TERM=xterm-24bits
+        fi
+      '';
+    };
+  }
 
   system.defaults.dock.autohide = true;
   system.defaults.dock.launchanim = false;
