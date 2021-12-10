a5a3f9b81059f5c104b6c174b98f78150acd7e27
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-10 20:58:39 +0000

    build: update compilation errors

--- install.sh
+++ install.sh
@@ -4,30 +4,35 @@ echo "installing Nix"
 curl -L https://nixos.org/nix/install | sh
 
 echo "installing Homebrew"
-/bin/bash -c \
-"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+exec zsh --login
+
+echo "downloading dotfiles"
+mkdir ~/Developer
+cd Developer
+git clone https://github.com/sciencefidelity/dotfiles
+cd ..
 
 echo "installing nix-darwin"
 nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
 ./result/bin/darwin-installer
 
 echo "add home-manager module"
-nix-channel --add \
-https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 
-# TODO - can we use nix-to load git without installing it?
-echo "downloading dotfiles"
-nix-env -iA nixpkgs.git
-mkdir ~/Developer
-cd Developer
-git clone https://github.com/sciencefidelity/dotfiles
-cd ..
-nix-env -e git
+exec zsh --login
 
 echo "creating simlinks"
-ln -s ~/Developer/dotfiles/nixpkgs/darwin-configuration.nix \
-~/.nixpkgs/darwin-configuration.nix
+rm ~/.nixpkgs/darwin-configuration.nix
+sudo mv /ect/nix/nix.conf /etc/nix/nix.conf.bak
+sudo mv /etc/shells /etc/shells.bak
+ln -s ~/Developer/dotfiles/.terminfo ~/.terminfo
+ln -s ~/Developer/dotfiles/nixpkgs/darwin-configuration.nix ~/.nixpkgs/darwin-configuration.nix
 
-echo "building nix-darwin and home-manager"
+echo "building nix-darwin"
 darwin-rebuild switch
+
+echo "restarting"
+sudo shutdown -r now
