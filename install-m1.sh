0401731dfcf4bc4417d9d069c1a2c80fc76fb925
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-12-14 22:43:09 +0000

    build: create install script for macbook m1 pro

---
+++ install-m1.sh
@@ -0,0 +1,28 @@
+#! bin/zsh
+
+echo "installing Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "downloading dotfiles"
+mkdir ~/Developer
+cd Developer
+git clone https://github.com/sciencefidelity/dotfiles
+cd ..
+
+echo "symlinking dotfiles"
+ln -s ~/Developer/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/config/zsh ~/.config/zsh
+ln -s ~/Developer/dotfiles/config/bat ~/.config/bat
+ln -s ~/Developer/dotfiles/config/emacs.d/init.el ~/.emacs.d/init.el
+ln -s ~/Developer/dotfiles/config/nvim/init.lua ~/.config/nvim/init.lua
+ln -s ~/Developer/dotfiles/config/starship/starship.toml ~/.config/starship/starship.toml
+ln -s ~/Developer/dotfiles/.terminfo ~/.terminfo
+ln -s ~/Developer/dotfiles/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -s ~/Developer/dotfiles/config/hammerspoon/init.lua ~/.hammerspoon/init.lua
+ln -s ~/Developer/dotfiles/.gitconfig ~/.gitconfig
+
+echo "copying system files"
+ln -s /Users/matt/Library/Mobile\ Documents/com~apple~CloudDocs/Developer/ssh ~/.ssh
+cp ~/Developer/dotfiles/pictures/desktop/* ~/Pictures/
+sudo cp ~/Developer/dotfiles/pictures/user/* /Library/User\ Pictures/Fun
+cp /Users/matt/Library/Mobile\ Documents/com~apple~CloudDocs/System/MonoLisa/* /Users/matt/Library/Fonts/
