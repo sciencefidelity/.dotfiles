c8808ed1dbf4a82ed6a75dce305900c87e485a00
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-15 17:48:42 +0000

    update install file for intell mac

--- install.sh
+++ install.sh
@@ -1,16 +1,10 @@
 #! bin/zsh
 
-echo "installing Nix"
-curl -L https://nixos.org/nix/install | sh
-
 echo "installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo "downloading dotfiles"
-mkdir ~/Developer
-cd Developer
-git clone https://github.com/sciencefidelity/dotfiles
-cd ..
+echo "installing Nix"
+curl -L https://nixos.org/nix/install | sh
 
 echo "installing nix-darwin"
 nix-build https://github.com/LnL7/nix-darwin/archive/master.tar.gz -A installer
@@ -20,6 +14,16 @@ echo "add home-manager module"
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 
+echo "backing up Nix files that are created by nix-darwin"
+sudo mv /ect/nix/nix.conf /etc/nix/nix.conf.bak
+sudo mv /etc/shells /etc/shells.bak
+
+echo "downloading dotfiles"
+mkdir ~/Developer
+cd Developer
+git clone https://github.com/sciencefidelity/dotfiles
+cd ..
+
 echo "creating simlinks"
 rm ~/.nixpkgs/darwin-configuration.nix
 sudo mv /ect/nix/nix.conf /etc/nix/nix.conf.bak
@@ -27,6 +31,12 @@ sudo mv /etc/shells /etc/shells.bak
 ln -s ~/Developer/dotfiles/.terminfo ~/.terminfo
 ln -s ~/Developer/dotfiles/nixpkgs/darwin-configuration.nix ~/.nixpkgs/darwin-configuration.nix
 ln -s /Users/matt/Library/Mobile\ Documents/com~apple~CloudDocs/Developer/ssh ~/.ssh
+
+rm ~/.nixpkgs/darwin-configuration.nix
+ln -s ~/Developer/dotfiles/nixpkgs/darwin-configuration-m1.nix ~/.nixpkgs/darwin-configuration.nix
+ln -s ~/Developer/dotfiles/.terminfo ~/.terminfo
+ln -s ~/Library/Mobile\ Documents/com~apple~CloudDocs/Developer/ssh ~/.ssh
+mkdir ~/.nvm
 
 echo "installing basic system files"
 cp /Users/matt/Library/Mobile\ Documents/com~apple~CloudDocs/System/Desktop/* ~/Pictures/
