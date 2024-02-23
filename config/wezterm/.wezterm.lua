7a4a5a0e769f0712d982bf8dddfca3b285355976
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-02-23 23:55:30 +0000

    refactor: change theme to catppuccin

--- config/wezterm/.wezterm.lua
+++ config/wezterm/.wezterm.lua
@@ -1,31 +1,34 @@
-local wezterm = require 'wezterm'
+local wezterm = require "wezterm"
 
-local config = {}
-
-config.font = wezterm.font 'MonoLisa'
-config.font_size = 13.0
-config.line_height = 1.17
-config.freetype_load_target = "Light"
-config.freetype_interpreter_version = 40
-config.bold_brightens_ansi_colors = "No"
-config.color_scheme = "Dracula (Official)"
-config.enable_tab_bar = false
-config.tab_bar_at_bottom = true
-config.use_fancy_tab_bar = false
-config.tab_max_width = 8
-config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "INTEGRATED_BUTTONS"
-config.initial_cols = 223
-config.initial_rows = 52
-config.cell_width = 0.9
-config.window_padding = {
-  left = 19,
-  right = 19,
-  top = 80,
-  bottom = 10,
+return {
+  font = wezterm.font_with_fallback { "MonoLisa Custom", "Symbols Nerd Font Mono" },
+  font_size = 13.0,
+  line_height = 1.17,
+  freetype_load_target = "Light",
+  freetype_interpreter_version = 40,
+  bold_brightens_ansi_colors = "No",
+  -- color_scheme = "Dracula (Official)",
+  color_scheme = "Catppuccin Mocha",
+  enable_tab_bar = false,
+  tab_bar_at_bottom = true,
+  use_fancy_tab_bar = false,
+  tab_max_width = 8,
+  hide_tab_bar_if_only_one_tab = true,
+  window_decorations = "INTEGRATED_BUTTONS",
+  initial_cols = 223,
+  initial_rows = 52,
+  cell_width = 0.9,
+  window_padding = {
+    left = 19,
+    right = 19,
+    top = 80,
+    bottom = 10,
+  },
+  inactive_pane_hsb = {
+    saturation = 1.0,
+    brightness = 1.0,
+  },
+  default_cursor_style = "SteadyUnderline",
+  animation_fps = 1,
+  audible_bell = "Disabled",
 }
-config.default_cursor_style = "SteadyUnderline"
-config.animation_fps = 1
-config.audible_bell = "Disabled"
-
-return config
