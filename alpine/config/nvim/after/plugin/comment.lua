ddf270b54163b1a827229c6aa32791230e2d8d9c
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-03-20 09:59:29 +0000

    feat: start applications modules and add specific home for nixbook

--- alpine/config/nvim/after/plugin/comment.lua
+++
@@ -1,4 +0,0 @@
-require("Comment").setup({})
-
-local comment_ft = require("Comment.ft")
-comment_ft.set("lua", { "--%s", "--[[%s--]]" })
--- alpine/config/nvim/after/plugin/filetype/elixir.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "ex", "exs" },
-  callback = function()
-    vim.opt_local.tabstop = 2
-    vim.opt_local.softtabstop = 2
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.expandtab = true
-  end,
-})
