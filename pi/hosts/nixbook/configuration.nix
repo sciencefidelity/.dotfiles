5b41a2658420371f6030da19ac9b7d24169ae4b6
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-03-20 17:19:16 +0000

    fix: add nixbook hardware configuration

--- pi/hosts/nixbook/configuration.nix
+++ pi/hosts/nixbook/configuration.nix
@@ -7,14 +7,39 @@
   ];
 
   boot = {
-    kernel.sysctl = {
-      "vm.swappiness" = 10;
-      "vm.vsf_cache_pressure" = 50;
-    };
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
+    kernel.sysctl = {
+      "vm.swappiness" = 10;
+      "vm.vsf_cache_pressure" = 50;
+    };
+    tmp.cleanOnBoot = true;
+    kernalParams = [
+      "hid_apple.fnmode= 2"
+      "hid_apple.swap_fn_leftctrl=1"
+    ];
+  };
+
+  environment = {
+    systemPackages = with pkgs; [
+      hyprland
+      kitty
+      swww
+    ];
+  };
+
+  fonts = {
+    fontDir.enable = true;
+    enableDefaultPackages = true;
+    packages = with pkgs; [
+      inconsolata
+    ];
+  };
+
+  programs = {
+    hyprland.enable = true;
   };
 }
 
