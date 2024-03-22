09f7ece61835df0f4ca4c0b6089ec4eab2f271ea
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-03-22 14:28:17 +0000

    fix: update key commands for yabai

--- hosts/macbook/configuration.nix
+++ hosts/macbook/configuration.nix
@@ -21,8 +21,8 @@ in
   };
 
   fonts = {
-    fontconfig.enable = true;
-    packages = with pkgs; [
+    fontDir.enable = true;
+    fonts = with pkgs; [
       (stdenv.mkDerivation {
         name = "monolisa";
         src = fetchGit {
@@ -134,70 +134,6 @@ in
 
   services = {
     nix-daemon.enable = true;
-
-    skhd = {
-      enable = true;
-      skhdConfig = ''
-        alt - j : yabai -m window --focus south
-        alt - k : yabai -m window --focus north
-        alt - h : yabai -m window --focus west
-        alt - j : yabai -m window --focus east
-        alt - s : yabai -m display --focus west
-        alt - g : yabai -m display --focus east
-        shift + alt - r : yabai -m space --rotate 270
-        shift + alt - y : yabai -m space --mirror y-axis
-        shift + alt - x : yabai -m space --mirror x-axis
-        shift + alt - t : yabai -m window --toggle float --grid 4:4:1:1:2:2
-        shift + alt - m : yabai -m window --toggle zoom-fullscreen
-        shift + alt - e : yabai -m space --balance
-        shift + alt - j : yabai -m window --swap south
-        shift + alt - k : yabai -m window --swap north
-        shift + alt - h : yabai -m window --swap west
-        shift + alt - j : yabai -m window --swap east
-        ctrl + alt - j : yabai -m window --warp south
-        ctrl + alt - k : yabai -m window --warp north
-        ctrl + alt - h : yabai -m window --warp west
-        ctrl + alt - j : yabai -m window --warp east
-        shift + alt - s : yabai -m window --display west; yabai -m display --focus west
-        shift + alt - g : yabai -m window --display east; yabai -m display --focus east
-        shift + alt - p : yabai -m window --space prev
-        shift + alt - j : yabai -m window --space next
-        shift + alt - 1 : yabai -m window --space 1
-        shift + alt - 2 : yabai -m window --space 2
-        shift + alt - 3 : yabai -m window --space 3
-        shift + alt - 4 : yabai -m window --space 4
-        shift + alt - 5 : yabai -m window --space 5
-        shift + alt - 6 : yabai -m window --space 6
-        shift + alt - 7 : yabai -m window --space 7
-        ctrl + alt - q : brew services stop yabai
-        ctrl + alt - s : brew services start yabai
-      '';
-    };
-
-    yabai = {
-      enable = true;
-      config = {
-        layout              = "bsp";
-        focus_follows_mouse = "autoraise";
-        mouse_follows_focus = "off";
-        window_placement    = "second_child";
-        window_opacity      = "off";
-        top_padding         = 7;
-        bottom_padding      = 7;
-        left_padding        = 7;
-        right_padding       = 7;
-        window_gap          = 7;
-        mouse_modifier      = "alt";
-        mouse_action1       = "move";
-        mouse_action2       = "resize";
-        mouse_drop_action   = "swap";
-      };
-      extraConfig = ''
-        yabai -m rule --add app="^System Settings$" manage=off
-        yabai -m rule --add app="^Calculator$" manage=off
-        yabai -m rule --add app="^Karabiner-Elements$" manage=off
-      '';
-    };
   };
 
   system = {
---
+++ modules/services/yabai/default.nix
@@ -0,0 +1,30 @@
+{ ... }:
+
+{
+  services = {
+    yabai = {
+      enable = true;
+      config = {
+        layout              = "bsp";
+        focus_follows_mouse = "autoraise";
+        mouse_follows_focus = "off";
+        window_placement    = "second_child";
+        window_opacity      = "off";
+        top_padding         = 7;
+        bottom_padding      = 7;
+        left_padding        = 7;
+        right_padding       = 7;
+        window_gap          = 7;
+        mouse_modifier      = "alt";
+        mouse_action1       = "move";
+        mouse_action2       = "resize";
+        mouse_drop_action   = "swap";
+      };
+      extraConfig = ''
+        yabai -m rule --add app="^System Settings$" manage=off
+        yabai -m rule --add app="^Calculator$" manage=off
+        yabai -m rule --add app="^Karabiner-Elements$" manage=off
+      '';
+    };
+  };
+}
