e1c8b70211e1b75439e4dda3699738a39264e858
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-05-19 22:11:01 +0100

    feat: add tcpdump to linux machines

--- modules/packages/nixpkgs/default.nix
+++ modules/packages/nixpkgs/default.nix
@@ -17,7 +17,7 @@ in
       unzip
       vscode-langservers-extracted
     ] ++ (if platform == "darwin" then [ ]
-    else if platform == "linux" then [ pkgs.lemonade pkgs.unrar-free pkgs.xclip pkgs.xsel ]
+    else if platform == "linux" then [ pkgs.lemonade pkgs.tcpdump pkgs.unrar-free pkgs.xclip pkgs.xsel ]
     else [ ]);
   };
 }
