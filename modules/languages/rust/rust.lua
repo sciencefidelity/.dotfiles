1585bf82b9b186998d9227f5a5310eca595e939f
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-05-29 00:14:15 +0100

    fix: remove incorrect rust_analyzer config option

--- modules/languages/rust/rust.lua
+++ modules/languages/rust/rust.lua
@@ -17,7 +17,6 @@ require("lspconfig").rust_analyzer.setup({
     settings = {
       diagnostics = { enable = false },
       cargo = { features = "all" },
-      checkOnSave = { command = "clippy" },
     },
   },
 })
