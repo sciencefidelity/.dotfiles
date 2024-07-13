f2a7aa229b6240f73373d67aa1e898f084cf11c2
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-07-14 00:44:23 +0100

    fix: update direnv global options

--- modules/programs/direnv/default.nix
+++ modules/programs/direnv/default.nix
@@ -1,15 +1,24 @@
 { ... }:
 
 {
-  home.file = {
-    direnv = {
-      enable = true;
-      target = ".config/direnv/direnv.toml";
-      text = /*toml*/ ''
-        [global]
-        # https://direnv.net/man/direnv.toml.1.html
-        hide_env_diff = true
-      '';
+  home = {
+    file = {
+      direnv = {
+        enable = true;
+        target = ".config/direnv/direnv.toml";
+        text = /*toml*/ ''
+          [global]
+          # https://direnv.net/man/direnv.toml.1.html
+          disable_stdin = true
+          load_dotenv = true
+          strict_env = true
+          hide_env_diff = true
+        '';
+      };
+    };
+
+    sessionVariables = {
+      DIRENV_LOG_FORMAT = "";
     };
   };
 
