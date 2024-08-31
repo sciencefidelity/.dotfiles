67b7ace260192ffbcac64f66a44c07c622b6c844
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-09-01 00:24:09 +0100

    refactor: remove more mac apps

--- hosts/rhea/home.nix
+++ hosts/rhea/home.nix
@@ -10,8 +10,6 @@ in
     ./config.nix
     ../../base/home.nix
     ../../modules/applications/hyprland
-    # ../../modules/applications/waybar
-    # ../../modules/applications/wezterm
     ../../modules/applications/alacritty
   ];
 
--- modules/packages/homebrew/default.nix
+++ modules/packages/homebrew/default.nix
@@ -28,30 +28,21 @@
       "bartender"
       "brave-browser"
       "discord"
-      "figma"
       "firefox"
       "karabiner-elements"
-      "miro"
-      "notion"
       "obsidian"
-      "sigmaos"
       "slack"
       "spotify"
       "zoom"
       "wezterm"
-      "visual-studio-code"
     ];
 
     masApps = {
-      "Compressor" = 424390742;
       "Craft - Docs and Notes Editor" = 1487937127;
-      "Drafts" = 1435957248;
-      "Final Cut Pro" = 424389933;
       "iA Writer" = 775737590;
       "Microsoft Excel" = 462058435;
       "Microsoft PowerPoint" = 462062816;
       "Microsoft Word" = 462054704;
-      "OneDrive" = 823766827;
       "Refined GitHub" = 1519867270;
       "Save to Raindrop.io" = 1549370672;
       "Save to Reader" = 1640236961;
