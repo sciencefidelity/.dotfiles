0318f8ce92dddbcdbfee9f6612d4bef6fb440e77
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-10-12 23:32:18 +0100

    fix: disable tmp2 on pi

--- hosts/eris/configuration.nix
+++ hosts/eris/configuration.nix
@@ -9,6 +9,7 @@
   ];
 
   boot = {
+    initrd.systemd.enableTpm2 = false;
     kernelPackages = pkgs.linuxKernel.packages.linux_rpi4;
     kernelParams = [ "8250.nr_uarts=1" "console=ttyAMA0,115200" "console=tty1" ];
     loader = {
