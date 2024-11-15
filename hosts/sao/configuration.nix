b8656c07272bfa6b41229390b397dc50f4391779
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-11-15 19:45:12 +0000

    feat: add sqlite

--- hosts/sao/configuration.nix
+++ hosts/sao/configuration.nix
@@ -5,6 +5,7 @@
     ./hardware-configuration.nix
     ./config.nix
     ../../base/configuration.nix
+    ../../modules/services/sqlite
     ../../modules/virtualization/docker
   ];
 
@@ -23,7 +24,7 @@
   networking = {
     hostName = config.hostname;
     wireless.enable = false;
-    firewall.allowedTCPPorts = [ 22 80 3000 3030 5173 5432 8000 8080 ];
+    firewall.allowedTCPPorts = [ 22 80 3000 3030 5173 5432 8000 8080 30333 ];
   };
 
   services = {
