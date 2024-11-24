b53bf7ca2ba7b4f3ca9553d4bc3050842d58bead
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-11-24 21:34:59 +0000

    fix: svelte snippets

--- modules/languages/typescript/snippets.lua
+++ modules/languages/typescript/snippets.lua
@@ -1,34 +1,39 @@
 local ls = require("luasnip")
 local fmt = require("luasnip.extras.fmt").fmt
--- local conds_expand = require("luasnip.extras.conditions.expand")
+local conds_expand = require("luasnip.extras.conditions.expand")
 local s = ls.snippet
--- local t = ls.text_node
+local t = ls.text_node
 local i = ls.insert_node
 
 ls.add_snippets("typescript", {
   s(
     "pl",
-    [[
+    fmt(
+      [[
 import type {{ PageLoad }} from './$types';
 
-export const load: PageLoad = ({{ params }}) => {
+export const load: PageLoad = ({{ params }}) => {{
   return {{
 
   }};
 }};
-]]
+]],
+      {}
+    )
   ),
-  s("ste", fmt("struct {1};", { i(1) })),
   s(
     "sl",
-    [[
-import type { PageServerLoad } from './$types';
+    fmt(
+      [[
+import type {{ PageServerLoad }} from './$types';
 
-export const load: PageServerLoad = async ({ params }) => {
-  return {
+export const load: PageServerLoad = async ({{ params }}) => {{
+  return {{
 
-  };
-};
-]]
+  }};
+}};
+]],
+      { }
+    )
   ),
 })
