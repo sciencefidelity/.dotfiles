e9805a506329aaf66d4c8eca199f8b8dfb9ceb62
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2025-03-18 05:12:29 +0000

    chore: update flake

--- modules/services/redis/default.nix
+++ modules/services/redis/default.nix
@@ -10,8 +10,11 @@
   services.redis = {
     package = pkgs.redis;
     servers."augmentaero" = {
+      bind = null;
       enable = true;
+      openFirewall = true;
       port = 6379;
+      requirePass = "password";
     };
     vmOverCommit = true;
   };
