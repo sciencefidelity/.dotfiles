b71264346f0cf53d7c2b20df6782a070cef97c33
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2025-05-21 21:00:28 +0100

    refactor: improve rust flake template]

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -7,25 +7,35 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+        run = pkgs.writeShellScriptBin "run" ''
+          watchexec -e rs -r cargo run
+        '';
       in
       with pkgs;
       {
         devShells.default = mkShell {
           buildInputs = [
             pkg-config
+            run
+            taplo
+            watchexec
             (rust-bin.stable.latest.default.override {
               extensions = [ "rust-analyzer" "rust-src" ];
             })
           ];
 
           shellHook = /*bash*/ ''
+          ''
+          # enable mold linker for Linux
+          + pkgs.lib.optionalString pkgs.stdenv.isLinux /*bash*/ ''
+            export RUSTFLAGS="-C linker=clang -C link-arg=-fuse-ld=${pkgs.mold-wrapped}/bin/mold"
           '';
         };
       }
