296b35d69a6269d879097c6978cce370c5fb5b28
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2025-05-24 13:01:40 +0100

    fix: disable unknown zsh settings

--- modules/programs/zsh/default.nix
+++ modules/programs/zsh/default.nix
@@ -73,10 +73,10 @@ in
 
       bindkey -v
       bindkey "^ " autosuggest-accept
-      bindkey -M menuselect "h" vi-backward-char
-      bindkey -M menuselect "k" vi-up-line-or-history
-      bindkey -M menuselect "l" vi-forward-char
-      bindkey -M menuselect "j" vi-down-line-or-history
+      # bindkey -M menuselect "h" vi-backward-char
+      # bindkey -M menuselect "k" vi-up-line-or-history
+      # bindkey -M menuselect "l" vi-forward-char
+      # bindkey -M menuselect "j" vi-down-line-or-history
       bindkey -v "^?" backward-delete-char
       bindkey -s "^o" "lfcd\n"
       bindkey "^e" edit-command-line
